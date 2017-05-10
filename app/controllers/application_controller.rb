7dd80a5d78f89a6276095dc9424cfdab8ad08390
Jordi Fernandez <neowax@Jordi-Fernandezs-MacBook-Pro.local>
2017-05-10 13:52:47 +0100

    add turbolinks gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  
+  before_action :set_locale
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up)
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update)
+  end
+  
+  def set_locale #  i18n
+    I18n.locale = I18n.default_locale
+  end
+  
 end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  
+  skip_before_action :authenticate_user!, only: %i[index]
+  
+  def index
+  end
+  
+end
