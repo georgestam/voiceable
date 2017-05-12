4b94a06a309e0a3c2097290e77f6b52850e75dd8
Jordi Fernandez <neowax@Jordi-Fernandezs-MacBook-Pro.local>
2017-05-12 16:54:50 +0100

    Conf index, show and update actions for basic api implementation

---
+++ app/controllers/api/respira/v1/recordings_controller.rb
@@ -0,0 +1,37 @@
+class Api::Respira::V1::RecordingsController < Api::Respira::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_recording, only: [ :show, :update ]
+  
+  def index
+  @recordings = policy_scope(Recording)
+  end
+
+  def show
+  end
+
+  def update
+    if @recording.update(recording_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_recording
+    @recording = Recording.find(params[:id])
+    authorize @recording 
+  end
+
+  def recording_params
+    params.require(:recording).permit(:data, :description)
+  end
+
+  def render_error
+    render json: { errors: @recording.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  
+end
