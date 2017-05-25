d3b0c5bd6ddf19e7924e38a07ec360b6bed6c5ea
Jordi Fernandez <jordi@ywarp.com>
2017-05-26 01:03:08 +0200

    Basic i18n implementation in home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,29 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
+  
+  scope '(:locale)', locale: /en|es/ do
 
-  root to: "pages#index"
-  
-  get 'about', to: "pages#about"
-  get 'contribute', to: "pages#contribute"
-  get 'contact', to: "pages#contact"
-  get 'questions', to: "pages#questions"
-  
-  scope '/dashboard' do
-    root to: "users#main", as: :dashboard
-    get "welcome", to: "users#welcome", as: :welcome
-  end
-  
-  namespace :api, defaults: { format: :json } do
-    namespace :respira do
-      namespace :v1 do
-        resources :recordings, only: %i[index show update]
+    root to: "pages#index"
+    
+    get 'about', to: "pages#about"
+    get 'contribute', to: "pages#contribute"
+    get 'contact', to: "pages#contact"
+    get 'questions', to: "pages#questions"
+    
+    scope '/dashboard' do
+      root to: "users#main", as: :dashboard
+      get "welcome", to: "users#welcome", as: :welcome
+    end
+    
+    namespace :api, defaults: { format: :json } do
+      namespace :respira do
+        namespace :v1 do
+          resources :recordings, only: %i[index show update]
+        end
       end
     end
+    
   end
   
 end
